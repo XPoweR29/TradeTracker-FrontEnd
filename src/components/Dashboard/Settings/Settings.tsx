562a069ab46e8a3f35da946983472e42c0eff128
XPoweR29 <xpower-29@wp.pl>
2023-03-16 15:20:56 +0100

    added simple views of Settings and Statistics

---
+++ src/components/Dashboard/Settings/Settings.tsx
@@ -0,0 +1,9 @@
+import styles from './Settings.module.scss';
+
+export const Settings = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h1>Ustawienia</h1>
+        </div>
+    )
+}
---
+++ src/components/Dashboard/Statisctics/Statisctics.tsx
@@ -0,0 +1,10 @@
+import styles from './Statistics.module.scss';
+
+export const Statistics = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h1>Statystyki!</h1>
+        </div>
+
+    )
+}
