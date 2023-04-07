80c37107bd5476e39c097bb64edad4cbb3b32b49
XPoweR29 <xpower-29@wp.pl>
2023-04-07 13:36:17 +0200

    Added charts and calculations

--- src/components/Dashboard/Statisctics/Statisctics.tsx
+++ src/components/Dashboard/Statisctics/Statisctics.tsx
@@ -1,9 +1,13 @@
+import { MonthlyChart } from './AllTimeChart/MonthlyChart';
+import { Effectivness } from './Effectivness/Effectivness';
 import styles from './Statistics.module.scss';
 
 export const Statistics = () => {
+
     return (
         <div className={styles.wrapper}>
-            <h1>Statystyki!</h1>
+                <MonthlyChart/>
+                <Effectivness/>
         </div>
 
     )
