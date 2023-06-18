fbc0b88880d94006a3b1dbe184f2b66516a78b59
XPoweR29 <xpower-29@wp.pl>
2023-06-18 17:17:16 +0200

    Created logout function that make query to logout api action

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -7,6 +7,7 @@ import {IoSettingsSharp} from 'react-icons/io5';
 import {BiLogOut} from 'react-icons/bi';
 import { AppContext } from '../../Common/Contexts/AppContext';
 import { Confirm } from '../../Common/Confirm/Confirm';
+import { apiUrl } from '../../../config/api';
 
 interface Props {
     setTitle: (val: string) => void;
@@ -15,6 +16,16 @@ interface Props {
 export const Sidebar = (props: Props) => {
     const {setIsAuthenticated} = useContext(AppContext)!;
     const [showConfirm, setShowConfirm] = useState(false);
+
+    const logout = async() => {
+        const rawRes = await fetch(`${apiUrl}/api/auth/logout`, {credentials: 'include'});
+        if(rawRes.status === 401) {
+            setIsAuthenticated(false);
+        }
+        //FIXME: W każdym zapytaniu do backendu dodać przechwytywanie błedu 401 i wylogowac użytkownika. 
+        setIsAuthenticated(false);
+        console.log(await rawRes.json());
+    }
 
     return(
     <div className={styles.wrapper}>
@@ -56,10 +67,11 @@ export const Sidebar = (props: Props) => {
         {showConfirm && 
         <Confirm
             message='Czy napewno chcesz się wylogować?'
-            onConfirm={()=>setIsAuthenticated(false)}
+            onConfirm={logout}
             showConfirm={setShowConfirm}
         />
         }
     </div>
     ) 
-}
+};
+
